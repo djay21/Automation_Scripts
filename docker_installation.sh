e3d85aff17fc313bb1e0119585cdc2b138a37719
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-01 16:30:10 +0530

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 
+
+os=$(hostnamectl | grep Operating)
+if [[ $cc == *"Ubuntu"* ]]; then 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 docker --version
+
+else
+sudo yum update -y
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.119.1-1.c57a6f9.el7.noarch.rpm
+#sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.55-1.el7.noarch.rpm
+sudo yum install docker-ce
+sudo usermod -a -G docker $USER
+sudo systemctl start docker
+fi
+
+
 
 echo "Docker-compose installation"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -18,3 +33,6 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 exit 0
+
+
+
