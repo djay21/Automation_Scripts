7043da5b7ae15fcf1e314df1ebc4b4eeb63a7917
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-11 09:59:45 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,6 +1,9 @@
+cat <<EOT >> ~/.bashrc
 alias pp='docker ps -a'
 alias im='docker images -a'
 function logs() { docker logs -f "$1" }
 function up() { docker-compose up -d }
 function exe() { docker exec -it "$1" /bin/bash ;}
 function builder() { docker build -t "$1" . ;}
+EOT
+source ~/.bashrc
