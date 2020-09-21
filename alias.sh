03a28566d498a7003bbedbfe28ffd2a11b523696
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-21 11:07:49 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,4 +1,5 @@
 wget https://raw.githubusercontent.com/djay21/Automation_Scripts/master/alias.txt
+sudo usermod -aG docker $USER
 sed -i '/^alias/d' ~/.bashrc
 cat alias.txt >> ~/.bashrc
 source ~/.bashrc
