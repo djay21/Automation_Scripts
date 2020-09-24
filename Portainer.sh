29d0442bccae67c69daf0e5d2b16faa7633378db
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-24 17:57:25 +0530

    Create Portainer.sh

---
+++ Portainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt install apache2-utils -y
+read -p "\n\nEnter your password: " pass
+echo -e "\nPortainer Password is ${pass}"
+password=$(htpasswd -nb -B admin ${pass} | cut -d ":" -f 2)
+echo "Encrypted password is : $password"
+docker run -d -p 8004:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer --admin-password ${password} || sudo docker run -d -p 8004:9000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer --admin-password ${password}
