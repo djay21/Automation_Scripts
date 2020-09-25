1ad532cd0b0aff41ce98c9239e3b5a6df4f3978e
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-09-25 16:03:19 +0530

    Update proxy_setup.sh

--- proxy_setup.sh
+++ proxy_setup.sh
@@ -1,4 +1,20 @@
 read -p "Enter the proxy address(like: http://proxy:8080/) : " proxy
+
+sed -i "/^http/d" /etc/environment
+sed -i "/^ftp/d" /etc/environment
+sed -i "/^no_proxy/d" /etc/environment
+sed -i "/^HTTP/d" /etc/environment
+
+
+cat <<EOF >> /etc/environment
+http_proxy=$proxy
+https_proxy=$proxy
+ftp_proxy=$proxy
+no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+HTTP_PROXY=$proxy
+HTTPS_PROXY=$proxy
+EOF
+
 rm -f ~/.docker/config.json
 mkdir -p ~/.docker
 cd ~/.docker 
