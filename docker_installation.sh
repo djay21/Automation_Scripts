6118bfac32b1e46f0089565700c56415ee63ad9d
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2022-01-12 14:00:39 +0530

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -5,7 +5,7 @@ os=$(hostnamectl | grep Operating)
 echo "******************************************"
 echo $os
 
-if [[ $os == *"Ubuntu"* ]] || || [[ $os == *"Debian"* ]]; 
+if [[ $os == *"Ubuntu"* ]] || [[ $os == *"Debian"* ]]; 
 then 
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update -y
